e79a679740a8e6f3f09011c4d337bc0ab81ee5f4
Ayesha Quddus <gaf56ruq@rhrk.uni-kl.de>
2025-05-14 10:28:22 +0200

    Revert "Added manager constructor test: FAILED"
    This reverts commit dab1c9416e215d5037047cd2aae0e9f61c6c312a.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ayesha/VDSProject_Group2
-# Build directory: /home/ayesha/VDSProject_Group2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("/home/ayesha/VDSProject_Group2/gtest/googletest-build")
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ayesha/VDSProject_Group2/src
-# Build directory: /home/ayesha/VDSProject_Group2/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
