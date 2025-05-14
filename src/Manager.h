f65212da13d5abbe3848b92358a36a0307be7ab1
Duong Pham <duong24111@gmail.com>
2025-05-14 07:47:42 +0700

    modify CMakeLists and src/Manager

--- src/Manager.h
+++ src/Manager.h
@@ -6,8 +6,75 @@
 #define VDSPROJECT_MANAGER_H
 
 #include "ManagerInterface.h"
+#include <vector>
+#include "Node.h"
+
 
 namespace ClassProject {
+
+    class Manager : public ManagerInterface {
+        public:
+            std::vector<Node> unique_table;
+
+            /**
+             * Constructor of Manager class
+             */
+
+            Manager();
+
+            /**
+             * Create a new Variable with input label
+             *
+             * @param label String label
+             * @return ID of BDD Node
+             */
+            BDD_ID createVar(const std::string &label) override;
+
+            const BDD_ID &True() override;
+
+            const BDD_ID &False() override;
+
+            bool isConstant(BDD_ID f) override;
+
+            bool isVariable(BDD_ID x) override;
+
+            BDD_ID topVar(BDD_ID f) override;
+
+            BDD_ID ite(BDD_ID i, BDD_ID t, BDD_ID e) override;
+
+            BDD_ID coFactorTrue(BDD_ID f, BDD_ID x) override;
+
+            BDD_ID coFactorFalse(BDD_ID f, BDD_ID x) override;
+
+            BDD_ID coFactorTrue(BDD_ID f) override;
+
+            BDD_ID coFactorFalse(BDD_ID f) override;
+
+            BDD_ID and2(BDD_ID a, BDD_ID b) override;
+
+            BDD_ID or2(BDD_ID a, BDD_ID b) override;
+
+            BDD_ID xor2(BDD_ID a, BDD_ID b) override;
+
+            BDD_ID neg(BDD_ID a) override;
+
+            BDD_ID nand2(BDD_ID a, BDD_ID b) override;
+
+            BDD_ID nor2(BDD_ID a, BDD_ID b) override;
+
+            BDD_ID xnor2(BDD_ID a, BDD_ID b) override;
+
+            std::string getTopVarName(const BDD_ID &root) override;
+
+            void findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root) override;
+
+            void findVars(const BDD_ID &root, std::set<BDD_ID> &vars_of_root) override;
+
+            size_t uniqueTableSize() override;
+
+            void visualizeBDD(std::string filepath, BDD_ID &root) override;
+
+    };
 
 }
 
