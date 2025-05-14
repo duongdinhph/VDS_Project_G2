68759e70edb2cfb100b60311558a997e69c458bc
TalhaIsrar <talhaisrar02@gmail.com>
2025-05-14 12:29:40 +0200

    Added extra test cases for TRUE/FALSE

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -17,12 +17,22 @@ TEST_F(ManagerTest, constructor_size_test){
 
 /// True Node
 TEST_F(ManagerTest, true_test){
-    EXPECT_EQ(1,managerTest.True());
+    BDD_ID True_ID = managerTest.True();
+    EXPECT_EQ(1,True_ID);
+    EXPECT_EQ("TRUE",managerTest.unique_table[True_ID].label);
+    EXPECT_EQ(1,managerTest.unique_table[True_ID].high);
+    EXPECT_EQ(1,managerTest.unique_table[True_ID].low);
+    EXPECT_EQ(1,managerTest.unique_table[True_ID].top_var);
 }
 
 /// False Node
 TEST_F(ManagerTest, false_test){
-    EXPECT_EQ(0,managerTest.False());
+    BDD_ID False_ID = managerTest.False();
+    EXPECT_EQ(0,False_ID);
+    EXPECT_EQ("FALSE",managerTest.unique_table[False_ID].label);
+    EXPECT_EQ(0,managerTest.unique_table[False_ID].high);
+    EXPECT_EQ(0,managerTest.unique_table[False_ID].low);
+    EXPECT_EQ(0,managerTest.unique_table[False_ID].top_var);
 }
 
 /// Manager Size
