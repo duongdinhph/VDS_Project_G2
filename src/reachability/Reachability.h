c5f091b33d0a043cdcf57b6ff890ab9655ec2906
Duong Pham <duong24111@gmail.com>
2025-06-23 20:51:18 +0200

    create Reachability.h, add private variables

--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -5,9 +5,31 @@
 
 namespace ClassProject {
 
-//    class Reachability : public ReachabilityInterface {
-//
-//    };
+    class Reachability : public ReachabilityInterface {
+    public:
+        // Constructor
+        Reachability(unsigned int stateSize, unsigned int inputSize);
+
+        // Destructor 
+        ~Reachability() override = default;
+
+        // Implementations of the virtual function
+        const std::vector<BDD_ID> &getStates() const override;
+        const std::vector<BDD_ID> &getInputs() const override;
+        bool isReachable(const std::vector<bool> &stateVector) override;
+        int stateDistance(const std::vector<bool> &stateVector) override;
+        void setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) override;
+        void setInitState(const std::vector<bool> &stateVector) override;
+    
+    private:
+        int numStates;
+        std::vector<BDD_ID> stateBits;
+        std::vector<BDD_ID> nextStateBits;
+        std::vector<BDD_ID> inputBits;
+        std::vector<BDD_ID> transitionFunctions;
+        
+        std::vector<bool> initState;
+    };
 
 }
 #endif
